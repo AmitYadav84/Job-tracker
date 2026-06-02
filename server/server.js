061f6c4b0708fe32e5706319c99b9f435a75e2ee
Amit Yadav <ayadav30031@gmail.com>
2026-06-02 17:11:41 +0530

    Initial frontend , backend and databse setup

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
+dotenv.config();
+connectDB();
+
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/health",(req,res) =>{
+    res.send("Server is healthy");
+});
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
